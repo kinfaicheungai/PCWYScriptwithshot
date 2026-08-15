961cae4b187314c0f0765ec459371ede31e2ede0
kinfaicheungai <kinfaicheung.ai@gmail.com>
2026-08-16 09:08:52 +1000

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pcwy-storyboard-v3";
+const CACHE = "pcwy-storyboard-v4";
 const SHELL = ["/", "/manifest.webmanifest", "/pwa-192.png", "/pwa-512.png"];
 
 self.addEventListener("install", event => {
